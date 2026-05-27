7b38b9e48d325b010c298b05c331dfe0e0d4c681
ManuGale <133926506+ManuGale@users.noreply.github.com>
2026-05-26 21:24:55 -0500

    Add console main menu

--- src/main/kotlin/menu/Menu.kt
+++ src/main/kotlin/menu/Menu.kt
@@ -4,29 +4,11 @@ class Menu {
 
     fun showMainMenu() {
 
-        var option: Int
-
-        do {
-
-            println("===== UNIT CONVERTER =====")
-            println("1. Temperature")
-            println("2. Length")
-            println("3. Weight")
-            println("4. Currency")
-            println("5. Exit")
-
-            print("Select an option: ")
-            option = readln().toInt()
-
-            when(option) {
-                1 -> println("Temperature module")
-                2 -> println("Length module")
-                3 -> println("Weight module")
-                4 -> println("Currency module")
-                5 -> println("Goodbye")
-                else -> println("Invalid option")
-            }
-
-        } while(option != 5)
+        println("===== UNIT CONVERTER =====")
+        println("1. Temperature")
+        println("2. Length")
+        println("3. Weight")
+        println("4. Currency")
+        println("5. Exit")
     }
 }
