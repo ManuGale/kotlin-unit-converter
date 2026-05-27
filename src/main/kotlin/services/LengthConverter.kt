bed3f71077a77dbf5cfd713396e2952d038e52ae
Carlos <l21530240@cancun.tecnm.mx>
2026-05-26 20:59:07 -0500

    Añadir conversion de kilometros a metros

--- src/main/kotlin/services/LengthConverter.kt
+++ src/main/kotlin/services/LengthConverter.kt
@@ -5,4 +5,8 @@ class LengthConverter {
     fun metersToKilometers(meters: Double): Double {
         return meters / 1000
     }
+
+    fun kilometersToMeters(km: Double): Double {
+        return km * 1000
+    }
 }
