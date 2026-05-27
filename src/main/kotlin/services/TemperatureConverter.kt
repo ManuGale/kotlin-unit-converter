aff0064d7ae587f20dfc78c68c24cc92caaf0781
Gabriel Cetz <gcetz33@gmail.com>
2026-05-26 22:48:23 -0500

    Add Fahrenheit to Celsius conversion

--- src/main/kotlin/services/TemperatureConverter.kt
+++ src/main/kotlin/services/TemperatureConverter.kt
@@ -4,4 +4,7 @@ class TemperatureConverter {
     fun celsiusToFahrenheit(celsius: Double): Double {
         return (celsius * 9/5) + 32
     }
+    fun fahrenheitToCelsius(fahrenheit: Double): Double {
+        return (fahrenheit - 32) * 5/9
+    }
 }
