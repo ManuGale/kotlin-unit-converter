28edee29b2238110bcbe24fa648c581a1dff516c
Emilio <l21530303@cancun.tecnm.mx>
2026-05-26 22:16:29 -0500

    Add EUR conversion functions

--- src/main/kotlin/services/CurrencyConverter.kt
+++ src/main/kotlin/services/CurrencyConverter.kt
@@ -9,4 +9,12 @@ class CurrencyConverter {
     fun mxnToUsd(mxn: Double): Double {
         return mxn / 18.5
     }
+
+    fun eurToMxn(eur: Double): Double {
+        return eur * 21.0
+    }
+
+    fun mxnToEur(mxn: Double): Double {
+        return mxn / 21.0
+    }
 }
