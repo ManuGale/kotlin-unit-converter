358fe9c521de47eb070875e6523842f549b5e4ab
Emilio <l21530303@cancun.tecnm.mx>
2026-05-26 22:14:52 -0500

    Add USD and MXN conversion functions

--- src/main/kotlin/services/CurrencyConverter.kt
+++ src/main/kotlin/services/CurrencyConverter.kt
@@ -1,4 +1,12 @@
 package services
 
 class CurrencyConverter {
+
+    fun usdToMxn(usd: Double): Double {
+        return usd * 18.5
+    }
+
+    fun mxnToUsd(mxn: Double): Double {
+        return mxn / 18.5
+    }
 }
